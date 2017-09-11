c39e87c30da7768ffc3da1225ba653b40615f703
Nguyen FnF <nguyenth@dasanzhone.com>
2017-09-11 17:33:31 +0700

    Add resolve domain name function.

--- server/controllers/queueBlacklist.controller.js
+++ server/controllers/queueBlacklist.controller.js
@@ -1,17 +1,19 @@
-var appRoot = require('app-root-path');
-var config = require('config.json');
-var express = require('express');
-var router = express.Router();
-var queueBlacklistService = require('services/queueBlacklist.service');
+const appRoot = require('app-root-path');
+const config = require('config.json');
+const express = require('express');
+const router = express.Router();
+const queueBlacklistService = require('services/queueBlacklist.service');
 const profileFacade = require('../model/profile/facade');
-var mongoose = require('mongoose');
-var async = require('async');
+const mongoose = require('mongoose');
+const async = require('async');
+const Address4 = require('ip-address').Address4;
+const dns = require('dns');
 
 const fileService = require('services/file.service');
 
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
-var fs = require('fs');
+const fs = require('fs');
 
 // const dest = 'amqp://localhost';
 const RABBIT_DEST = 'amqp://test:test@10.72.0.163:5672';
@@ -40,6 +42,11 @@ function add(req, res) {
     let blackIps = [];
 
     // 1st para in async.each() is the array of items
+    if (!selectedProfileIds.trim() || arrSelectedProfileIds.length <= 0) {
+        res.status(400).send("There isn't selected profiles.");
+        return;
+    }
+
     async.each(arrSelectedProfileIds,
         // 2nd param is the function that each item is passed to
         function (item, callback) {
@@ -74,58 +81,80 @@ function add(req, res) {
             const filename = queueItemId + ".txt";
 
             let fileContent = '';
-            blackIps.forEach(function(element) {
-                fileContent += element.blackIp + '\r\n';
-            }, this);
 
-            const filePath = fileService.DIR_UPLOAD_ABSOLUTE + '/blacklist/' + filename;
-            fs.writeFileSync(filePath, fileContent, 'utf8');
+            // blackIps.forEach(function (element) {
+            //     fileContent += element.blackIp + '\r\n';
+            // }, this);
 
-            //const url = encodeURIComponent(config.apiUrl + '/api/devices/downloadBlacklist?filename=' + filename);
-            const url = config.apiUrl + '/api/devices/downloadBlacklist?filename=' + filename;
+            // 1st para in async.each() is the array of items
+            async.each(blackIps,
+                // 2nd param is the function that each item is passed to
+                function (item, callback) {
+                    let dest = item.blackIp.trim();
+                    var address = new Address4(dest);
 
-            const json_queueBlacklist = {
-                id: queueItemId,
-                app: 'Blacklist',
-                deviceId: deviceId,
-                redirectContent: redirectContent,
-                type: 'UpdateConfig',
-                // date: '2017-08-04T07:29:27.474Z',
-                request: {
-                    url: url,
-                    md5: '239847sodkfjs23434kdsjfsdf2332',
-                    username: 'abc',
-                    password: '123456',
-                    fileSize: 12345
+                    if (address.isValid()) {
+                        fileContent += dest + '\r\n';
+                        callback();
+                    } else {
+                        dns.lookup(dest, function (err, result) {
+                            fileContent += result + '\r\n';
+                            callback();
+                        });
+                    }
+                },
+                // 3rd param is the function to call when everything's done
+                function (err) {
+                    const filePath = fileService.DIR_UPLOAD_ABSOLUTE + '/blacklist/' + filename;
+                    fs.writeFileSync(filePath, fileContent, 'utf8');
+
+                    //const url = encodeURIComponent(config.apiUrl + '/api/devices/downloadBlacklist?filename=' + filename);
+                    const url = config.apiUrl + '/api/devices/downloadBlacklist?filename=' + filename;
+
+                    const json_queueBlacklist = {
+                        id: queueItemId,
+                        app: 'Blacklist',
+                        deviceId: deviceId,
+                        redirectContent: redirectContent,
+                        type: 'UpdateConfig',
+                        // date: '2017-08-04T07:29:27.474Z',
+                        request: {
+                            url: url,
+                            md5: '239847sodkfjs23434kdsjfsdf2332',
+                            username: 'abc',
+                            password: '123456',
+                            fileSize: 12345
+                        }
+                    };
+
+                    amqp.connect(RABBIT_DEST, function (err, conn) {
+                        conn.createChannel(function (err, ch) {
+                            var queueName = 'CMD_INPUT';
+
+                            ch.assertQueue(queueName, { durable: true });
+
+                            ch.sendToQueue(queueName,
+                                new Buffer(JSON.stringify(json_queueBlacklist)) //,
+                                //{ persistent: true }
+                            );
+
+                            console.log(" [x] Sent '%s'", JSON.stringify(json_queueBlacklist));
+                        });
+
+                        setTimeout(function () {
+                            conn.close();
+
+                            // process.exit(0);
+                        }, 500);
+                    });
+
+                    queueBlacklistService.create(json_queueBlacklist).then(function () {
+                        res.send(json_queueBlacklist);
+                    }).catch(function (err) {
+                        res.status(400).send(err);
+                    });
                 }
-            };
-
-            amqp.connect(RABBIT_DEST, function (err, conn) {
-                conn.createChannel(function (err, ch) {
-                    var queueName = 'CMD_INPUT';
-
-                    ch.assertQueue(queueName, { durable: true });
-
-                    ch.sendToQueue(queueName,
-                        new Buffer(JSON.stringify(json_queueBlacklist)) //,
-                        //{ persistent: true }
-                    );
-
-                    console.log(" [x] Sent '%s'", JSON.stringify(json_queueBlacklist));
-                });
-
-                setTimeout(function () {
-                    conn.close();
-
-                    // process.exit(0);
-                }, 500);
-            });
-
-            queueBlacklistService.create(json_queueBlacklist).then(function () {
-                res.send(json_queueBlacklist);
-            }).catch(function (err) {
-                res.status(400).send(err);
-            });
+            );
         }
     );
 }
