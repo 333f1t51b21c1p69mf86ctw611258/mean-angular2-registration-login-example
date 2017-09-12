e670d0f90a743e6262e8ce7de22c6c24cf38a471
Nguyen FnF <nguyenth@dasanzhone.com>
2017-09-12 18:59:03 +0700

    Validate unique with profileName.

--- server/model/profile/schema.js
+++ server/model/profile/schema.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 
 const profileSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique : true },
   description: { type: String },
   blackIps: [{
     blackIp: String,
